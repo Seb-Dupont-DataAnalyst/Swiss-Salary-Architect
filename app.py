a4ee9fe840d5834031b89e4be7e40d6c760a02db
Sebastien Dupont <79512309+Seb-Dupont-DataAnalyst@users.noreply.github.com>
2026-06-14 17:37:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, jsonify
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
